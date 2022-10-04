67bb7236320087ac702cec9501c127f76951928c
minnim1010 <pms1010123@daum.net>
2022-10-04 15:25:12 +0900

    TEST: Connection to datasource

---
+++ src/test/java/com/example/board/DataSourceTest.java
@@ -0,0 +1,32 @@
+package com.example.board;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.java.Log;
+
+@Log
+@SpringBootTest
+public class DataSourceTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnectionToDataSource() {
+
+        try {
+            Connection con = dataSource.getConnection();
+            log.info("Meta_Data: " + con.getMetaData());
+            log.info("Schema: " + con.getSchema());
+            log.info("driver: " + con.getMetaData().getDriverName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
